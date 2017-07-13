11a16808e53e3e848342f4fc86b32666b9720cc0
admin@extentsoft.com <admin@extentsoft.com>
2017-07-13 16:46:38 +0700

    Initialzied gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require(‘gulp’); 
+var jshint = require(‘gulp-jshint’); 
+
+gulp.task(‘lint’, function(){       
+  return gulp.src(‘webapp/public/javascripts/*.js’)
+    .pipe(jshint())              
+    .pipe(jshint.reporter(‘default’));
+});  
+gulp.task(‘default’, [‘lint’]);
